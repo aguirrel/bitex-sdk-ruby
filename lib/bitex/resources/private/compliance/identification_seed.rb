69aee4164f92427900838dd2e0b5438b310f1af2
Juan Szymczak <juanangelszymczak@gmail.com>
2018-12-03 11:28:59 -0300

    Metrics/LineLength rubocop fix

--- lib/bitex/resources/private/compliance/identification_seed.rb
+++ lib/bitex/resources/private/compliance/identification_seed.rb
@@ -3,7 +3,8 @@ module Bitex
     module Compliance
       # pending doc TODO
       class IdentificationSeed < Private
-        # .create(identification_kind_code:, number:, issuer:, public_registry_authority:, public_registry_book:, public_registry_extra_data:)
+        # .create(identification_kind_code:, number:, issuer:, public_registry_authority:, public_registry_book:,
+        #   public_registry_extra_data:)
       end
     end
   end
--- lib/bitex/resources/private/compliance/natural_docket_seed.rb
+++ lib/bitex/resources/private/compliance/natural_docket_seed.rb
@@ -3,7 +3,8 @@ module Bitex
     module Compliance
       # pending doc TODO
       class NaturalDocketSeed < Private
-        # .create(first_name:, last_name:, nationality:, gender_code:, marital_status_code:, politically_exposed:, birth_date:, job_title:, job_description:, politically_exposed_reason:)
+        # .create(first_name:, last_name:, nationality:, gender_code:, marital_status_code:, politically_exposed:, birth_date:,
+        #   job_title:, job_description:, politically_exposed_reason:)
       end
     end
   end
