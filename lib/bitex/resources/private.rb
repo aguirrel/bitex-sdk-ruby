91897ef9cae66a278bb6f9e90185fa0416c3c0bc
Juan Szymczak <juanangelszymczak@gmail.com>
2018-12-03 11:00:52 -0300

    protected useless access modifier rubocop fix

--- lib/bitex/resources/private.rb
+++ lib/bitex/resources/private.rb
@@ -2,10 +2,12 @@ module Bitex
   module Resources
     # Generic base resource for private Bitex resources.
     class Private < Public
-      protected
+      class << self
+        protected
 
-      def self.build_connection(options = {})
-        Class.new(Connections::Private).tap { |connection| connection.api_key = options[:api_key] }
+        def build_connection(options = {})
+          Class.new(Connections::Private).tap { |connection| connection.api_key = options[:api_key] }
+        end
       end
     end
   end
--- lib/bitex/resources/public.rb
+++ lib/bitex/resources/public.rb
@@ -2,21 +2,21 @@ module Bitex
   module Resources
     # Generic base resource for public Bitex resources.
     class Public < JsonApiClient::Resource
-      def self.build(options = {})
-        Class.new(self).tap do |anonymous|
-          anonymous.connection_class = build_connection(options)
-          anonymous.parser = Parser
-          anonymous.site = "https://#{'sandbox.' if options[:sandbox]}bitex.la/api/"
-        end
-      end
-
-      protected
-
-      def self.build_connection(_options = {})
-        Class.new(Connections::Public)
-      end
-
       class << self
+        def build(options = {})
+          Class.new(self).tap do |anonymous|
+            anonymous.connection_class = build_connection(options)
+            anonymous.parser = Parser
+            anonymous.site = "https://#{'sandbox.' if options[:sandbox]}bitex.la/api/"
+          end
+        end
+
+        protected
+
+        def build_connection(_options = {})
+          Class.new(Connections::Public)
+        end
+
         def resource_name
           (name || superclass.name).demodulize.underscore
         end
