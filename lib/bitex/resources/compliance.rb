be3bc9b0d49b7e37e06aa62b2955001b4ca45295
Juan Szymczak <juanangelszymczak@gmail.com>
2018-12-04 16:54:00 -0300

    add compliance to autoload

---
+++ lib/bitex/resources/compliance.rb
@@ -0,0 +1,8 @@
+module Bitex
+  module Resources
+    # Experimental endpoint: This endpoint may not be supported in further releases of the API. If your integration relies on it,
+    # please contact us at developers@bitex.la explaining your use case, in order to provide you with better support.
+    module Compliance
+    end
+  end
+end
