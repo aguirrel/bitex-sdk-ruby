2836582a5d66218f9dbabc82f8fd25f2a4733f14
Juan Szymczak <juanangelszymczak@gmail.com>
2018-12-04 16:44:59 -0300

    add miscellaneous to autoload

--- lib/bitex/resources/miscellaneous.rb
+++ lib/bitex/resources/miscellaneous.rb
@@ -3,9 +3,6 @@ module Bitex
     # Experimental endpoint: This endpoint may not be supported in further releases of the API. If your integration relies on it,
     # please contact us at developers@bitex.la explaining your use case, in order to provide you with better support.
     module Miscellaneous
-      autoload :Movement, 'bitex/resources/private/miscellaneous/movement'
-      autoload :Account, 'bitex/resources/private/miscellaneous/account'
-      autoload :User, 'bitex/resources/private/miscellaneous/user'
     end
   end
 end
