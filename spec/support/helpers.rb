dac51d50569b2c12ea3dd753a77fb8207065a82a
Juan Szymczak <juanangelszymczak@gmail.com>
2018-12-05 12:34:40 -0300

    add write and read level client builders

--- spec/support/helpers.rb
+++ spec/support/helpers.rb
@@ -2,6 +2,14 @@ module Helpers
   extend ActiveSupport::Concern
 
   included do
+    def read_level_client
+      build_client(api_key: 'read_level')
+    end
+
+    def write_level_client
+      build_client(api_key: 'write_level')
+    end
+
     def build_client(api_key: nil, sandbox: true)
       Bitex::Client.new(api_key: api_key, sandbox: sandbox)
     end
