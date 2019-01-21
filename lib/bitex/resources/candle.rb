702b62b2519e77bf1b6d820ae9359506ef09fe0f
Juan Szymczak <juanangelszymczak@gmail.com>
2019-01-21 15:20:59 -0300

    improve specs and finders refactor for candles

--- lib/bitex/resources/candle.rb
+++ lib/bitex/resources/candle.rb
@@ -2,23 +2,17 @@ module Bitex
   module Resources
     # For market relationships.
     class Candle < Public
-      # /api/candles?filter[orderbook_code]={orderbook_code}&filter[days]={days}&span={span}
+      # GET /api/candles?filter[orderbook_code]={orderbook_code}&filter[days]={days}&span={span}
       #
       # Get candles for a given market
       #  filters:
-      #    @param [Base::Resource::Orderbook] orderbook.
+      #    @param [Orderbook] orderbook.
       #    @param [Integer] days. number of days from where you want the candles to be. default = 1
       #    @param [Integer] span. timespan for each candle. default = 1
       #
-      # @return JsonApiClient::ResultSet. It has the server response data, and all transactions parsed to json api.
-      def self.find(days: nil, orderbook: nil, span: nil)
-        filters = {}.tap do |params|
-          params[:days] = days if days.present?
-          params.merge!(orderbook_code: orderbook.code) if orderbook.present?
-        end
-
-        span.present? ? with_params(span: span).find(filters) : super(filters)
-      end
+      # @return [ResultSet<Candle>]
+      #
+      # .all(orderbook:, days:, span:)
     end
   end
 end
