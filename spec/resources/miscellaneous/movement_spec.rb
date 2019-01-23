e0406dc70d6c891bd1848679f145374c0f9ed9a1
Juan Szymczak <juanangelszymczak@gmail.com>
2019-01-23 15:21:38 -0300

    improve specs and finders refactor for movements

--- spec/resources/miscellaneous/movement_spec.rb
+++ spec/resources/miscellaneous/movement_spec.rb
@@ -1,26 +1,22 @@
 require 'spec_helper'
 
 describe Bitex::Resources::Miscellaneous::Movement do
-  shared_examples_for 'Movement' do
-    it { is_expected.to be_a(described_class) }
+  describe '.all', vcr: { cassette_name: 'movements/all' } do
+    subject(:movements) { client.movements.all }
 
-    its(:type) { is_expected.to eq(resource_name) }
-    its(:'attributes.keys') { is_expected.to contain_exactly(*%w[type id timestamp currencies_involved currency amount fee fee_decimals fee_currency price price_decimals kind]) }
-  end
+    it { is_expected.to be_a(JsonApiClient::ResultSet) }
 
-  describe '.all' do
-    subject { client.movements.all }
+    context 'taking a sample' do
+      subject(:sample) { movements.sample }
 
-    context 'with any level key', vcr: { cassette_name: 'movements/all' } do
-      let(:key) { read_level_key }
+      it { is_expected.to be_a(Bitex::Resources::Miscellaneous::Movement) }
 
-      it { is_expected.to be_a(JsonApiClient::ResultSet) }
-
-      context 'taking a sample' do
-        subject { super().sample }
-
-        it_behaves_like 'Movement'
-      end
+      its(:'attributes.keys') {
+        is_expected.to contain_exactly(
+          *%w[type id timestamp currencies_involved currency amount fee fee_decimals fee_currency price price_decimals kind]
+        )
+      }
+      its(:type) { is_expected.to eq('movements') }
     end
   end
 end
