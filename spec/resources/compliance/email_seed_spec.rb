4cfb8d709f56709ec5b92b85ac3d34b565a5af26
Juan Szymczak <juanangelszymczak@gmail.com>
2019-01-28 17:51:18 -0300

    add authorization with user for compliance:email seed

--- spec/resources/compliance/email_seed_spec.rb
+++ spec/resources/compliance/email_seed_spec.rb
@@ -2,10 +2,7 @@ require 'spec_helper'
 
 describe Bitex::Resources::Compliance::EmailSeed do
   describe '.create', vcr: { cassette_name: 'compliance/email_seeds/create' } do
-    subject { client.email_seeds.create(address: address, email_kind_code: email_kind_code) }
-
-    let(:address) { 'person@example.com' }
-    let(:email_kind_code) { 'invoicing' }
+    subject { client.email_seeds.create(601, address: 'sdk+invoicing@whitelabeler.com', email_kind_code: 'invoicing') }
 
     it { is_expected.to be_a(Bitex::Resources::Compliance::EmailSeed) }
 
@@ -14,7 +11,7 @@ describe Bitex::Resources::Compliance::EmailSeed do
     its(:type) { is_expected.to eq('email_seeds') }
     its(:id) { is_expected.to be_present }
 
-    its(:address) { is_expected.to eq(address) }
-    its(:email_kind_code) { is_expected.to eq(email_kind_code) }
+    its(:address) { is_expected.to eq('sdk+invoicing@whitelabeler.com') }
+    its(:email_kind_code) { is_expected.to eq('invoicing') }
   end
 end
