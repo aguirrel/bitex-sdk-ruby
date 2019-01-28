aec7247e5f3e47c91eafa25476c61f4867b3595b
Juan Szymczak <juanangelszymczak@gmail.com>
2019-01-28 13:45:55 -0300

    add Webhook resource

---
+++ lib/bitex/resources/webhook.rb
@@ -0,0 +1,19 @@
+module Bitex
+  module Resources
+    class Webhook < Private
+      # POST /api/webhooks
+      #
+      # @param [String] url. Your webhook.
+      #
+      # @return [Weebhook]
+      # 
+      # .create(url:)
+
+      # GET /aoi/webhoooks
+      #
+      # @return [Weebhook]
+      #
+      # .find
+    end
+  end
+end
