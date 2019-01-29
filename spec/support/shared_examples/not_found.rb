6823e1812bb584ae603153457a765609f7c36774
Juan Szymczak <juanangelszymczak@gmail.com>
2019-01-29 14:15:50 -0300

    remove deprecated shared examples

--- spec/support/shared_examples/not_found.rb
+++
@@ -1,5 +0,0 @@
-shared_examples_for 'Not Found' do
-  let(:id) { 99 }
-
-  it { expect { subject }.to raise_exception(JsonApiClient::Errors::NotFound) }
-end
--- spec/support/shared_examples/trading_bots.rb
+++
@@ -1,7 +0,0 @@
-shared_examples_for 'Trading Bots' do
-    it { is_expected.to be_a(described_class) }
-
-  its(:'attributes.keys') { is_expected.to contain_exactly(*%w[type id orderbook_code amount remaining_amount chunk_size eta executing to_cancel]) }
-  its(:type) { is_expected.to eq(resource_name) }
-  its(:'relationships.attributes.keys') { is_expected.to include(*%w[user]) }
-end
