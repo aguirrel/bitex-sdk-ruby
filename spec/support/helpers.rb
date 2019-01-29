3c6f7a98aab9181aabf1a877fc3e33585cd36654
Juan Szymczak <juanangelszymczak@gmail.com>
2019-01-29 14:18:08 -0300

    refactor client builder helper

--- spec/support/helpers.rb
+++ spec/support/helpers.rb
@@ -3,14 +3,11 @@ module Helpers
 
   included do
     def client
-      build_client(api_key: 'my_awesome_api_key', sandbox: false)
+      build_client(api_key: 'your_magic_api_key')
     end
 
     def build_client(api_key: nil, sandbox: true)
       Bitex::Client.new(api_key: api_key, sandbox: sandbox)
     end
-
-    let(:resource_name) { described_class.name.demodulize.underscore.pluralize }
-
   end
 end
