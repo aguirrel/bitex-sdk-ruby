fa0fd1a568cc4dbc4af55cac716d0dbc1ea6f202
Juan Szymczak <juanangelszymczak@gmail.com>
2019-01-31 23:34:02 -0300

    add properties for Ticker

--- lib/bitex/resources/ticker.rb
+++ lib/bitex/resources/ticker.rb
@@ -2,6 +2,16 @@ module Bitex
   module Resources
     # Overview of current market prices and trade volume, including Volume-Weighted Average Price.
     class Ticker < Public
+      property :low, type: :decimal
+      property :high , type: :decimal
+      property :open, type: :decimal
+      property :volume, type: :decimal
+      property :vwap , type: :decimal
+      property :ask, type: :decimal
+      property :bid, type: :decimal
+      property :last, type: :decimal
+      property :price_before_last, type: :decimal
+
       # GET /api/tickers/:orderbook_code
       #
       # Get Tickers of all available markets
