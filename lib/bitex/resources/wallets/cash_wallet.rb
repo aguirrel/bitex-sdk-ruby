5a0b1702f4ddafc57a50047f9a364e3e50bb371a
Juan Szymczak <juanangelszymczak@gmail.com>
2019-01-31 23:30:39 -0300

    add properties for Wallets

--- lib/bitex/resources/wallets/cash_wallet.rb
+++ lib/bitex/resources/wallets/cash_wallet.rb
@@ -3,6 +3,9 @@ module Bitex
     module Wallets
       # This resource has the user's balances in fiat currencies.
       class CashWallet < Wallet
+        property :balance, type: :decimal
+        property :available, type: :decimal
+
         # GET /api/cash_wallets
         #
         # @param [String] from.
