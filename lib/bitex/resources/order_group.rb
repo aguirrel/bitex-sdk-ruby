6e763318e8cd8efe580ba98e833f7ae5caa51456
Juan Szymczak <juanangelszymczak@gmail.com>
2019-01-31 23:34:34 -0300

    add properties for Market/OrderGroup

--- lib/bitex/resources/order_group.rb
+++ lib/bitex/resources/order_group.rb
@@ -2,6 +2,8 @@ module Bitex
   module Resources
     # For market relationships.
     class OrderGroup < Public
+      property :price, type: :decimal
+      property :amount, type: :decimal
     end
   end
 end
